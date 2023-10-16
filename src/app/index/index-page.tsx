da098f52c3167e221b4b612ac39a56fea45c07e6
keithmccall <keithcodes@gmail.com>
2023-10-15 19:20:36 -0700

    History base

--- src/app/index/index-page.tsx
+++ src/app/index/index-page.tsx
@@ -1,9 +1,16 @@
 import { ScheduledNotifications } from '@components/scheduled-notfications';
 import { Scheduler } from '@components/scheduler';
-import { makeStyles, useTheme } from '@rneui/themed';
+import { makeStyles, Text, useTheme } from '@rneui/themed';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+enum VIEW_MODE {
+  SCHEDULED = 'SCHEDULED',
+  HISTORY = 'HISTORY',
+}
 
 const useStyles = makeStyles((theme, props: any) => ({
   container: {
@@ -17,6 +24,7 @@ const useStyles = makeStyles((theme, props: any) => ({
 
 export const IndexPage = (props: any) => {
   const { theme } = useTheme();
+  const [viewMode, setViewMode] = useState<VIEW_MODE>(VIEW_MODE.SCHEDULED);
 
   return (
     <SafeAreaView
@@ -29,7 +37,81 @@ export const IndexPage = (props: any) => {
       <StatusBar style="inverted" />
       <ScrollView>
         <Scheduler />
-        <ScheduledNotifications />
+        <View
+          style={{
+            paddingVertical: 10,
+          }}
+        >
+          <View
+            style={{
+              borderTopWidth: 1,
+              borderTopColor: theme.colors.white,
+            }}
+          />
+        </View>
+        <View>
+          <View
+            style={{
+              flexDirection: 'row',
+
+              borderWidth: 3,
+              borderRadius: 10,
+              borderColor: theme.colors.grey5,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                padding: 10,
+              }}
+              onPress={() => {
+                setViewMode(VIEW_MODE.SCHEDULED);
+              }}
+              containerStyle={{
+                width: '50%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                ...(viewMode === VIEW_MODE.SCHEDULED && {
+                  borderStyle: 'solid',
+                  borderColor: theme.colors.grey5,
+                  backgroundColor: theme.colors.grey5,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                }),
+              }}
+            >
+              <Text>Scheduled</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                padding: 10,
+              }}
+              onPress={() => {
+                setViewMode(VIEW_MODE.HISTORY);
+              }}
+              containerStyle={{
+                width: '50%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                ...(viewMode === VIEW_MODE.HISTORY && {
+                  borderStyle: 'solid',
+                  borderColor: theme.colors.grey5,
+                  backgroundColor: theme.colors.grey5,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                }),
+              }}
+            >
+              <Text>History</Text>
+            </TouchableOpacity>
+          </View>
+          {viewMode === VIEW_MODE.SCHEDULED ? (
+            <ScheduledNotifications />
+          ) : (
+            <View>
+              <Text>HISTORY</Text>
+            </View>
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
