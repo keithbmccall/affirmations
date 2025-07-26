e844d287618e6b03d3a41d4663d5ba324239696e
keithmccall <keithcodes@gmail.com>
2025-07-25 23:23:18 -0700

    Basic camera

--- app/(tabs)/lens-screen.tsx
+++ app/(tabs)/lens-screen.tsx
@@ -1,30 +1,18 @@
-import { IconSymbol } from '@components';
+import { ThemedSafeAreaView } from '@components';
 import Lens from '@screen-containers/lens';
+import { globalStyles } from '@styles';
 import { StyleSheet } from 'react-native';
-import ParallaxScrollView from '../../.expo-defaults/components/ParallaxScrollView';
 
 export default function LensScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol size={310} color="#808080" name="camera.fill" style={styles.headerImage} />
-      }
-    >
+    <ThemedSafeAreaView style={styles.safeArea}>
       <Lens />
-    </ParallaxScrollView>
+    </ThemedSafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  safeArea: {
+    ...globalStyles.flex1,
   },
 });
