4c06e49ce5a2b9231b9746b999ab4b2e1cef48c8
keithmccall <keithcodes@gmail.com>
2025-08-01 14:13:15 -0700

    Unused

--- modules/color-lens-frame-processor/index.ts
+++
@@ -1,4 +0,0 @@
-// Reexport the native module. On web, it will be resolved to ColorLensFrameProcessorModule.web.ts
-// and on native platforms to ColorLensFrameProcessorModule.ts
-export { default } from './src/ColorLensFrameProcessor';
-export * from './src/ColorLensFrameProcessor.types';
