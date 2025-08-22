ab2778f46df2b9f6b60ba9b0379d9ec42c36e416
keithmccall <keithcodes@gmail.com>
2025-08-21 23:43:55 -0700

    Circulars and reminder of individual providers

--- lib/screen-containers/affirmations.tsx
+++ lib/screen-containers/affirmations.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Notifications } from '@features/notifications';
 import { ThemedView } from '@components/shared';
+import { Notifications } from '@features/notifications';
+import { StatusBar } from 'expo-status-bar';
 import { ScreenContainerProps } from './types';
 
 interface AffirmationsProps extends ScreenContainerProps {}
 
 const Affirmations = ({ statusBarProps }: AffirmationsProps) => {
+  // TODO: wrap with affirmations provider
   return (
     <ThemedView>
       <StatusBar {...statusBarProps} />
