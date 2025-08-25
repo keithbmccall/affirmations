36ba30f91f03454971bbaa745879d52cc27bafd0
keithmccall <keithcodes@gmail.com>
2025-08-24 22:30:19 -0700

    PaletteImage

--- lib/screen-containers/camera-roll-inspector.tsx
+++ lib/screen-containers/camera-roll-inspector.tsx
@@ -1,10 +1,14 @@
 import { Modal } from '@components/modal';
+import { router } from 'expo-router';
 import { Text } from 'react-native';
 import { ScreenContainerProps } from 'react-native-screens';
 
 interface CameraRollInspector extends ScreenContainerProps {}
 
 export const CameraRollInspector = () => {
+  const handleBack = () => {
+    router.back();
+  };
   return (
     <Modal title="Camera Roll Inspector" testID="camera-roll-inspector-title">
       <Text>Camera Roll Inspector</Text>
